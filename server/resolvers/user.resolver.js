c10f3b6c4741fa460bbf8c660a0e330cce3d30cf
Lawrence Li <lawrenceli3202@gmail.com>
2024-08-26 22:28:21 -0400

    feat: implement sign in for user resolvers

--- server/resolvers/user.resolver.js
+++ server/resolvers/user.resolver.js
@@ -1,15 +1,55 @@
+import bcrypt from "bcryptjs";
 import { users } from "../data/dummy.js";
+import User from "../models/user.model.js";
 
 const userResolver = {
+  Mutation: {
+    signUp: async (_, { input }, context) => {
+      try {
+        const { username, name, password, gender } = input;
+
+        if (!usename || !name || !password || !gender) {
+          throw new Error("Please fill all fields.");
+        }
+
+        const existingUser = await User.findOne({ usename });
+
+        if (existingUser) {
+          throw new Error("User already exists.");
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser = new User({
+          username,
+          name,
+          password: hashedPassword,
+          gender,
+          profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+        });
+
+        await newUser.save();
+        await context.login(newUser);
+
+        return newUser;
+      } catch (error) {
+        console.log("Error in creating user", error);
+        throw new Error(error.message || "Internal Server Error");
+      }
+    },
+  },
   Query: {
     users: (_, _, { req, res }) => {
       return users;
     },
     user: (_, { userId }) => {
-      return users.find((user) => user.id === userId);
+      return users.find((user) => user._id === userId);
     },
   },
-  Mutation: {},
 };
 
 export default userResolver;
