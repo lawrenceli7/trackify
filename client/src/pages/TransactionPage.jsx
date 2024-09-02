fbc13af35f11bc8919139e2d0c5f92723f65b2e6
Lawrence Li <lawrenceli3202@gmail.com>
2024-09-01 21:10:21 -0400

    chore: add loading state animation

--- client/src/pages/TransactionPage.jsx
+++ client/src/pages/TransactionPage.jsx
@@ -8,10 +8,11 @@ import {
   Flex,
   Form,
   Input,
+  message,
   Row,
   Select,
+  Spin,
   Typography,
-  message,
 } from "antd";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
@@ -40,6 +41,7 @@ const { Title, Text } = Typography;
 const TransactionPage = () => {
   const { id } = useParams();
   const [form] = Form.useForm();
+  const [isLoading, setLoading] = useState(false);
 
   const { loading, data } = useQuery(GET_TRANSACTION, {
     variables: { id: id },
@@ -63,8 +65,10 @@ const TransactionPage = () => {
 
   const handleSubmit = async () => {
     const amount = parseFloat(formData.amount);
+    setLoading(true);
 
     try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
       await updateTransaction({
         variables: {
           input: {
@@ -78,6 +82,8 @@ const TransactionPage = () => {
     } catch (error) {
       console.error(error.message);
       message.error("Fail to update transaction");
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -127,155 +133,162 @@ const TransactionPage = () => {
           Update this Transaction
         </Title>
         <Divider />
-        <Form
-          name="basic"
-          form={form}
-          layout="vertical"
-          onFinish={handleSubmit}
-          autoComplete="off"
-        >
-          <Form.Item
-            label="Transaction"
-            rules={[
-              {
-                required: true,
-                message: "Please input the transaction description!",
-              },
-            ]}
+        <Spin spinning={isLoading} tip="Loading">
+          <Form
+            name="basic"
+            form={form}
+            layout="vertical"
+            onFinish={handleSubmit}
+            autoComplete="off"
           >
-            <Input
-              placeholder="Rent, Groceries, Salary, etc."
-              prefix={<FaMoneyBillWave className="text-gray-400" />}
-              value={formData.description}
-              onChange={handleInputChange}
-              name="description"
-            />
-          </Form.Item>
-          <Row gutter={16}>
-            <Col span={8}>
-              <Form.Item
-                label="Payment Type"
-                rules={[
-                  { required: true, message: "Please select a payment type!" },
-                ]}
-              >
-                <Select
-                  placeholder="Select Payment Type"
-                  value={formData.paymentType}
-                  onChange={(value) => handleSelectChange(value, "paymentType")}
-                  name="paymentType"
-                >
-                  <Option value={"card"}>
-                    <Flex className="flex items-center gap-2">
-                      <FaCreditCard className="text-gray-400" />
-                      <Text>Card</Text>
-                    </Flex>
-                  </Option>
-                  <Option value={"cash"}>
-                    <Flex className="flex items-center gap-2">
-                      <FaMoneyBill className="text-gray-400" />
-                      <Text>Cash</Text>
-                    </Flex>
-                  </Option>
-                </Select>
-              </Form.Item>
-            </Col>
-            <Col span={8}>
-              <Form.Item
-                label="Category"
-                rules={[
-                  { required: true, message: "Please select a category!" },
-                ]}
-              >
-                <Select
-                  placeholder="Select Category"
-                  value={formData.category}
-                  onChange={(value) => handleSelectChange(value, "category")}
-                  name="category"
-                >
-                  <Option value={"saving"}>
-                    <Flex className="flex items-center gap-2">
-                      <MdSavings className="text-gray-400" />
-                      <Text>Saving</Text>
-                    </Flex>
-                  </Option>
-                  <Option value={"expense"}>
-                    <Flex className="flex items-center gap-2">
-                      <GiExpense className="text-gray-400" />
-                      <Text>Expense</Text>
-                    </Flex>
-                  </Option>
-                  <Option value={"investment"}>
-                    <Flex className="flex items-center gap-2">
-                      <MdRealEstateAgent className="text-gray-400" />
-                      <Text>Investment</Text>
-                    </Flex>
-                  </Option>
-                </Select>
-              </Form.Item>
-            </Col>
-            <Col span={8}>
-              <Form.Item
-                label="Amount ($)"
-                rules={[
-                  { required: true, message: "Please enter the amount!" },
-                ]}
-              >
-                <Input
-                  type="number"
-                  placeholder="100"
-                  prefix={<FaCashRegister className="text-gray-400" />}
-                  value={formData.amount}
-                  onChange={handleInputChange}
-                  name="amount"
-                />
-              </Form.Item>
-            </Col>
-          </Row>
-          <Row gutter={16}>
-            <Col span={12}>
-              <Form.Item
-                label="Location"
-                rules={[
-                  { required: true, message: "Please enter the location!" },
-                ]}
-              >
-                <Input
-                  placeholder="Enter location..."
-                  prefix={<FaMapMarkerAlt className="text-gray-400" />}
-                  value={formData.location}
-                  onChange={handleInputChange}
-                  name="location"
-                />
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item
-                label="Date"
-                rules={[{ required: true, message: "Please select a date!" }]}
-              >
-                <DatePicker
-                  className="w-full"
-                  placeholder="Select date..."
-                  suffixIcon={<FaCalendarAlt className="text-gray-400" />}
-                  value={formData.date ? dayjs(formData.date) : null}
-                  onChange={handleDateChange}
-                />
-              </Form.Item>
-            </Col>
-          </Row>
-          <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="w-full"
-              icon={<GrUpdate />}
-              disabled={loadingUpdate}
+            <Form.Item
+              label="Transaction"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input the transaction description!",
+                },
+              ]}
             >
-              {loadingUpdate ? "Updating..." : "Update Transaction"}
-            </Button>
-          </Form.Item>
-        </Form>
+              <Input
+                placeholder="Rent, Groceries, Salary, etc."
+                prefix={<FaMoneyBillWave className="text-gray-400" />}
+                value={formData.description}
+                onChange={handleInputChange}
+                name="description"
+              />
+            </Form.Item>
+            <Row gutter={16}>
+              <Col span={8}>
+                <Form.Item
+                  label="Payment Type"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please select a payment type!",
+                    },
+                  ]}
+                >
+                  <Select
+                    placeholder="Select Payment Type"
+                    value={formData.paymentType}
+                    onChange={(value) =>
+                      handleSelectChange(value, "paymentType")
+                    }
+                    name="paymentType"
+                  >
+                    <Option value={"card"}>
+                      <Flex className="flex items-center gap-2">
+                        <FaCreditCard className="text-gray-400" />
+                        <Text>Card</Text>
+                      </Flex>
+                    </Option>
+                    <Option value={"cash"}>
+                      <Flex className="flex items-center gap-2">
+                        <FaMoneyBill className="text-gray-400" />
+                        <Text>Cash</Text>
+                      </Flex>
+                    </Option>
+                  </Select>
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="Category"
+                  rules={[
+                    { required: true, message: "Please select a category!" },
+                  ]}
+                >
+                  <Select
+                    placeholder="Select Category"
+                    value={formData.category}
+                    onChange={(value) => handleSelectChange(value, "category")}
+                    name="category"
+                  >
+                    <Option value={"saving"}>
+                      <Flex className="flex items-center gap-2">
+                        <MdSavings className="text-gray-400" />
+                        <Text>Saving</Text>
+                      </Flex>
+                    </Option>
+                    <Option value={"expense"}>
+                      <Flex className="flex items-center gap-2">
+                        <GiExpense className="text-gray-400" />
+                        <Text>Expense</Text>
+                      </Flex>
+                    </Option>
+                    <Option value={"investment"}>
+                      <Flex className="flex items-center gap-2">
+                        <MdRealEstateAgent className="text-gray-400" />
+                        <Text>Investment</Text>
+                      </Flex>
+                    </Option>
+                  </Select>
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="Amount ($)"
+                  rules={[
+                    { required: true, message: "Please enter the amount!" },
+                  ]}
+                >
+                  <Input
+                    type="number"
+                    placeholder="100"
+                    prefix={<FaCashRegister className="text-gray-400" />}
+                    value={formData.amount}
+                    onChange={handleInputChange}
+                    name="amount"
+                  />
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col span={12}>
+                <Form.Item
+                  label="Location"
+                  rules={[
+                    { required: true, message: "Please enter the location!" },
+                  ]}
+                >
+                  <Input
+                    placeholder="Enter location..."
+                    prefix={<FaMapMarkerAlt className="text-gray-400" />}
+                    value={formData.location}
+                    onChange={handleInputChange}
+                    name="location"
+                  />
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item
+                  label="Date"
+                  rules={[{ required: true, message: "Please select a date!" }]}
+                >
+                  <DatePicker
+                    className="w-full"
+                    placeholder="Select date..."
+                    suffixIcon={<FaCalendarAlt className="text-gray-400" />}
+                    value={formData.date ? dayjs(formData.date) : null}
+                    onChange={handleDateChange}
+                  />
+                </Form.Item>
+              </Col>
+            </Row>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="w-full"
+                icon={<GrUpdate />}
+                disabled={loadingUpdate}
+              >
+                {loadingUpdate ? "Updating..." : "Update Transaction"}
+              </Button>
+            </Form.Item>
+          </Form>
+        </Spin>
       </Card>
     </Flex>
   );
