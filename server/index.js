e9f9b114937619052d4315758ca91f83b0eb84a1
Lawrence Li <lawrenceli3202@gmail.com>
2024-09-01 20:23:27 -0400

    build: prepare deploy

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import session from "express-session";
 import { buildContext } from "graphql-passport";
 import http from "http";
 import passport from "passport";
+import path from "path";
 import { connectDB } from "./db/connectDB.js";
 import { configurePassport } from "./passport/passport.config.js";
 import mergedResolvers from "./resolvers/index.js";
@@ -17,6 +18,7 @@ import mergedTypeDefs from "./typeDefs/index.js";
 dotenv.config();
 configurePassport();
 
+const __dirname = path.resolve();
 const app = express();
 const httpServer = http.createServer(app);
 const MongoDBStore = connectMongo(session);
@@ -65,6 +67,12 @@ app.use(
     context: async ({ req, res }) => buildContext({ req, res }),
   })
 );
+
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
+});
 
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
 await connectDB();
